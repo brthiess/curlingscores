291651610a2d6d0e728dc8a157b055e5baccfb13
Brad <brthiess@ualberta.ca>
2017-10-18 21:44:27 -0600

    13th commit

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -206,6 +206,12 @@ function showSlide(id){
 	$("[data-slide-id]").removeClass("active");
 	$("[data-slide-id=" + id + "]").addClass("active");
 }
+$(document).ready(function() {
+	$(".slide-wrapper").slick({
+		arrows: false,
+		dots: true
+	});
+});
 /********
 Functions 
 *********/
@@ -223,3 +229,37 @@ function ordinal_suffix_of(i) {
 	}
 	return i + "th";
 }
+
+
+/* scoreboard swiping */
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;                                                        
+
+function handleTouchStart(evt) {                                         
+    xDown = evt.touches[0].clientX;                                      
+    yDown = evt.touches[0].clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+           console.log(evt)
+        }                      
+    } 
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
