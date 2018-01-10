393ae8f042c24f738f2eab178c289c80dd32ac23
Brad <brthiess@ualberta.ca>
2018-01-09 20:44:17 -0700

    first

--- routes/page-routes.js
+++ routes/page-routes.js
@@ -5,17 +5,5 @@ var controller = require('../controllers/page-controller');
 /* GET home page. */
 router.get('/', controller.index);
 
-router.get('/schedule/:category?/:year?', controller.schedule);
-
-router.get('/scores', controller.scores);
-
-router.get('/rankings/:showType?/:category?/:year?', controller.rankings);
-
-router.get('/teams/:teamId([0-9]{1,50})', controller.team);
-router.get('/teams/:category?', controller.teams);
-
-router.get('/contact', controller.contact);
-
-router.get('/events/:eventId', controller.event);
 
 module.exports = router;
